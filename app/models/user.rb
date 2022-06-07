e23fc1058262b150a73248a9a953b3febf0bf371
nO2xygen <vladkom25112000@gmail.com>
2022-06-07 03:44:02 +0300

    added active admin config + devise fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-    devise :database_authenticatable,
-      :validatable,
-      :jwt_authenticatable,
-      :registerable,
-      jwt_revocation_strategy: JwtDenylist
+  devise :database_authenticatable,
+    :validatable,
+    :jwt_authenticatable,
+    :registerable,
+    jwt_revocation_strategy: JwtDenylist
+  self.skip_session_storage = [:http_auth, :params_auth]
   
-    belongs_to :role, polymorphic: true 
+  belongs_to :role, polymorphic: true 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,28 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-Specialty.create(name: 'Dentist')
-Specialty.create(name: 'Ophthalmologist')
-Specialty.create(name: 'Cardiologist')
+# Specialty.create(name: 'Dentist')
+# Specialty.create(name: 'Ophthalmologist')
+# Specialty.create(name: 'Cardiologist')
 
-@doctor = Doctor.new(name: 'Den Dent', specialty_id: 1, user_attributes: {email: 'dendent@gmail.com', password: '12345678'})
-@doctor.user.save!
-@doctor.save
+# @doctor = Doctor.new(name: 'Den Dent', specialty_id: 1, user_attributes: {email: 'dendent@gmail.com', password: '12345678'})
+# @doctor.user.save!
+# @doctor.save
 
-@doctor = Doctor.new(name: 'Steve Eye', specialty_id: 2, user_attributes: {email: 'steveeye@gmail.com', password: '12345678'})
-@doctor.user.save!
-@doctor.save
+# @doctor = Doctor.new(name: 'Steve Eye', specialty_id: 2, user_attributes: {email: 'steveeye@gmail.com', password: '12345678'})
+# @doctor.user.save!
+# @doctor.save
 
-@doctor = Doctor.new(name: 'Megan Peek', specialty_id: 2, user_attributes: {email: 'meganpeek@gmail.com', password: '12345678'})
-@doctor.user.save!
-@doctor.save
+# @doctor = Doctor.new(name: 'Megan Peek', specialty_id: 2, user_attributes: {email: 'meganpeek@gmail.com', password: '12345678'})
+# @doctor.user.save!
+# @doctor.save
 
-@doctor = Doctor.new(name: 'Rick Cardo', specialty_id: 3, user_attributes: {email: 'rickcardo@gmail.com', password: '12345678'})
-@doctor.user.save!
-@doctor.save
+# @doctor = Doctor.new(name: 'Rick Cardo', specialty_id: 3, user_attributes: {email: 'rickcardo@gmail.com', password: '12345678'})
+# @doctor.user.save!
+# @doctor.save
 
-@patient = Patient.new(name: 'Vlad Kom', gender: 'male', age: '21', user_attributes: {email: 'v@k.com', password: '12345678'})
-@patient.user.save!
-@patient.save
+# @patient = Patient.new(name: 'Vlad Kom', gender: 'male', age: '21', user_attributes: {email: 'v@k.com', password: '12345678'})
+# @patient.user.save!
+# @patient.save
+
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
